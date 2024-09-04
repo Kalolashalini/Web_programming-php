3d4589514d15026f0d273585375cfe69657bd32e
Kalolashalini <kalolashalini7@gmail.com>
2024-09-04 19:12:42 +0530

    Add files via upload

---
+++ Task1/script5.php
@@ -0,0 +1,9 @@
+<?php
+
+$firstname = "SHALINI"; 
+$lastname = "KALOLA";   
+
+
+echo "First Name: " . $firstname . "<br>";
+echo "Last Name: " . $lastname;
+?>
---
+++ Task1/script9.php
@@ -0,0 +1,15 @@
+<?php
+$name="Shalini";
+$age=21;
+$city="Rajkot";
+$dob="06th july 2004";
+
+
+echo "Name = $name <br>
+ Age = $age <br>
+ City = $city <br>
+ DOB = $dob";
+
+
+
+?>
